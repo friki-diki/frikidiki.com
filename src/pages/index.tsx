771a06e36c02eb2c7c4a122c4a47e620af43737f
Tosh Goodson <tosh.goodson@gmail.com>
2022-07-03 21:19:30 -0700

    auto play music; more content; hideous scrollbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,31 +3,69 @@ import Head from 'next/head'
 import Image from 'next/image'
 import logo from '../../assets/images/logo.webp'
 import logoRotating from '../../assets/images/logo-rotating.gif'
+import map from '../../assets/images/map.png'
 import favicon from '../../assets/images/favicon.png'
+import partyDancing from '../../assets/images/party-dancing.gif'
+import { FC, useEffect, useRef } from 'react'
+
+const PartyDancing: FC<{className?: string}> = ({className}) => {
+	return <div className={`max-w-[38px] ${className ?? ''}`}>
+		<Image src={partyDancing}/>
+	</div>
+}
 
 const Home: NextPage = () => {
+	const audioRef = useRef<HTMLAudioElement>(null)
+
+	useEffect(() => {
+		document.addEventListener('scroll', () => {
+			audioRef.current?.play()
+		})
+		document.addEventListener('click', () => {
+			audioRef.current?.play()
+		})
+	}, [])
+
 	return (
 		<div className=''>
-			<div style={{textAlign: 'center', color: '#4d0c79'}}>
+			<div className="text-center text-[#773F0A]">
 				<Head>
 					<title>Friki Diki</title>
 					<meta name="description" content="Visit Friki Diki at Burning Man in 2022!" />
 					<link rel="icon" href={favicon.src} />
 				</Head>
 
-				<div className="my-32 bg-[#FFF8EC] [border-image:url('../assets/images/bamboo-border-fixed-smaller.png')_25_repeat] border-y-[20px] [border-image-outset:5px_!important]">
-					<div className="max-w-4xl mx-auto py-4 px-4 flex justify-center items-center">
+				<audio ref={audioRef} src="/coral-buddies.mp3" autoPlay loop/>
+
+				<div className="mt-32 bg-[#FFF8EC] [border-image:url('../assets/images/bamboo-border-fixed-smaller.png')_25_repeat] border-y-[20px] [border-image-outset:5px_!important]">
+					<div className="max-w-4xl mx-auto p-4 flex justify-center items-center">
 						<div className="w-16 flex-none mr-12"><Image src={logoRotating}/></div>
 						<h1 className="text-6xl">Friki Diki Home Page</h1>
 						<div className="w-16 flex-none ml-12"><Image src={logoRotating}/></div>
 					</div>
 				</div>
 
-				<div style={{maxWidth: '512px', margin: 'auto'}}>
-					<Image src={logo} alt="Friki Diki"/>
-					<h1 style={{fontSize: '34px'}}>Visit Friki Diki on the playa 2022!</h1>
-					<p style={{fontSize: '20px'}}>Find us in Golden Guy alley @ D & 7:15</p>
-					<p style={{fontSize: '20px'}}>Tiki-themed drinks and fun!</p>
+				<div className="max-w-4xl mx-auto mt-8 mb-32 p-4 bg-[#FFF8EC] [border-image:url('../assets/images/bamboo-border-fixed-smaller.png')_25_repeat] border-[20px] [border-image-outset:5px_!important]">
+					<h2 className="text-5xl">Friki Diki is coming to the playa 2022!</h2>
+					<h3 className="text-2xl mt-2">Come for tropical drinks and fun at 7:15 &amp; Dali!</h3>
+					<div className="max-w-[420px] mx-auto mt-6">
+						<Image src={map}/>
+					</div>
+					<hr className="mt-12 [border-image:url('../assets/images/bamboo-border-fixed-smaller.png')_25_repeat] border-t-[10px]"/>
+					<hr className="mt-1 border-2 border-[#773F0A]"/>
+					<h3 className="text-3xl my-3 flex justify-center items-center">
+						<div className="flex-grow"><PartyDancing/></div>
+						<div className="flex-grow"><PartyDancing/></div>
+						<div className="flex-grow"><PartyDancing/></div>
+						<div className="flex-grow"><PartyDancing/></div>
+						<div className="px-2">!!Black light night on Wednesday!!</div>
+						<div className="flex-grow"><PartyDancing/></div>
+						<div className="flex-grow"><PartyDancing/></div>
+						<div className="flex-grow"><PartyDancing/></div>
+						<div className="flex-grow"><PartyDancing/></div>
+					</h3>
+					<hr className="border-2 border-[#773F0A]"/>
+					<hr className="mt-1 [border-image:url('../assets/images/bamboo-border-fixed-smaller.png')_25_repeat] border-t-[10px]"/>
 				</div>
 			</div>
 		</div>
