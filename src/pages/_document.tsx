525211ee718dc3af02cf60d2f271adbcf8cadbe1
Tosh Goodson <tosh.goodson@gmail.com>
2022-07-03 21:53:36 -0700

    custom cursor

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
 	return (
 		<Html>
 			<Head />
-			<body className="font-serif min-h-screen bg-[#F9E6B5] bg-[url('../assets/images/bg.png')] rendering-pixelated">
+			<body className="font-serif min-h-screen bg-[#F9E6B5] bg-[url('../assets/images/bg.png')] rendering-pixelated [cursor:url('../assets/images/palm-tree.gif'),auto]">
 				<Main />
 				<NextScript />
 			</body>
